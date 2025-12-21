1c1d7761d24c487abe66807fd82fba2aa8f3f004
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-21 10:31:06 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.3"
 
-  cluster_name    = "project10-eks"
+  cluster_name    = "project11-eks"
   cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
