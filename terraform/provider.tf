67cf5071ff30a377d316635510b330f7fb263cf9
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-28 11:48:54 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = var.aws_region
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
 }
